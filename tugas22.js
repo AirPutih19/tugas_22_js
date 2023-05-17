5776d21e876313edc60fd368915fec4b085f3905
AirPutih19 <kharismadwisj@gmail.com>
2023-05-17 15:01:52 +0700

    Tugas 22 JS
    Tugas 22 JS untuk niomic

---
+++ tugas22.js
@@ -0,0 +1,8 @@
+function panggilKata(){
+    var kata = ["Saya", "ingin", "belajar", "bersama"]
+    kata.forEach(function(item,index){
+        console.log("item : ",item,"Index ke ",index);
+        
+    })
+}
+panggilKata()
